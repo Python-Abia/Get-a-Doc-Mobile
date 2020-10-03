ede5463935eadb4acb4f2bb7ac80404464d6d483
Avishkar21 <avishkar.p@cumulations.com>
2020-10-03 06:20:04 +0530

    1. Added animated spalsh screen 2. After clicking back from login screen it will not move to Splash screen

--- app/src/main/java/com/example/getadocapp/MainActivity.java
+++ app/src/main/java/com/example/getadocapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.getadocapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,21 +9,43 @@ import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
 
+    MotionLayout motionLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        CountDownTimer countDownTimer = new CountDownTimer(3000,1000) {
+        motionLayout = findViewById(R.id.motion);
+        motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
             @Override
-            public void onTick(long l) {
+            public void onTransitionStarted(MotionLayout motionLayout, int i, int i1) {
 
             }
 
             @Override
-            public void onFinish() {
-            startActivity(new Intent(MainActivity.this,loginActivity.class));
+            public void onTransitionChange(MotionLayout motionLayout, int i, int i1, float v) {
+
             }
-        }; countDownTimer.start();
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int i) {
+                startActivity(new Intent(MainActivity.this,loginActivity.class));
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int i, boolean b, float v) {
+
+            }
+        });
+//        CountDownTimer countDownTimer = new CountDownTimer(3000,1000) {
+//            @Override
+//            public void onTick(long l) {
+//
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//            startActivity(new Intent(MainActivity.this,loginActivity.class));
+//            }
+//        }; countDownTimer.start();
     }
 }
